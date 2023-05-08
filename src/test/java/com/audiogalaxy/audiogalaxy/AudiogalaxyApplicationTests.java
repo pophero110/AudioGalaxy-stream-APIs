66885929bbf63315624219c8c55a55bc8794dc59
Jeff Ou <ojn2014520@gmail.com>
2023-05-08 16:14:27 -0500

    Initialize spring project

---
+++ src/test/java/com/audiogalaxy/audiogalaxy/AudiogalaxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audiogalaxy.audiogalaxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudiogalaxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
