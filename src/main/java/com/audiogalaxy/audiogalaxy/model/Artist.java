e8135bd2cb44d8570369361958ea60c2280747c9
wmavery <wanda.avery@att.net>
2023-05-08 17:06:35 -0500

    added Artist.java

--- src/main/java/com/audiogalaxy/audiogalaxy/model/Artist.java
+++ src/main/java/com/audiogalaxy/audiogalaxy/model/Artist.java
@@ -1,4 +1,51 @@
 package com.audiogalaxy.audiogalaxy.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="artists")
 public class Artist {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    public Artist() {
+    }
+
+    public Artist(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Artist(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
