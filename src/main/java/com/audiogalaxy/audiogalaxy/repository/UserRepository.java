42c80ffa8c97966dba4639a4913170fa5507b72b
Jeff Ou <ojn2014520@gmail.com>
2023-05-09 09:55:09 -0500

    Remove unused file and refactor code

--- src/main/java/com/audiogalaxy/audiogalaxy/repository/UserRepository.java
+++ src/main/java/com/audiogalaxy/audiogalaxy/repository/UserRepository.java
@@ -2,8 +2,9 @@ package com.audiogalaxy.audiogalaxy.repository;
 
 import com.audiogalaxy.audiogalaxy.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
