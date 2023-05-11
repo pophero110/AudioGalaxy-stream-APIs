7e8fb29f02c480412d04b53756aad93e173daf7f
Jeff Ou <ojn2014520@gmail.com>
2023-05-11 14:07:26 -0500

    Fix typo

--- src/test/java/com/audiogalaxy/audiogalaxy/PlaylistControllerTest.java
+++ src/test/java/com/audiogalaxy/audiogalaxy/PlaylistControllerTest.java
@@ -10,12 +10,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -27,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(PlaylistController.class)
-@Import(TestSecurityConfiguratoin.class)
+@Import(TestSecurityConfiguration.class)
 public class PlaylistControllerTest {
     @Autowired
     MockMvc mockMvc;
