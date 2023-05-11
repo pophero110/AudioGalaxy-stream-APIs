d77824220fdb7f23fc1bd2af053cff206aeda312
wmavery <wanda.avery@att.net>
2023-05-11 18:48:37 -0500

    updatge UserServiceTest.java to include check to ensure a user's account is active to loggin.

--- src/test/java/com/audiogalaxy/audiogalaxy/UserServiceTest.java
+++ src/test/java/com/audiogalaxy/audiogalaxy/UserServiceTest.java
@@ -1,6 +1,8 @@
 package com.audiogalaxy.audiogalaxy;
 
 import com.audiogalaxy.audiogalaxy.model.User;
+import com.audiogalaxy.audiogalaxy.model.request.LoginRequest;
+import com.audiogalaxy.audiogalaxy.model.response.LoginResponse;
 import com.audiogalaxy.audiogalaxy.repository.UserRepository;
 import com.audiogalaxy.audiogalaxy.service.UserService;
 import org.junit.Assert;
@@ -10,17 +12,24 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+
+import java.util.logging.Logger;
 
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class UserServiceTest {
+    Logger log = Logger.getLogger(UserServiceTest.class.getName());
 
     @MockBean
     UserRepository userRepository;
 
     @Autowired
     UserService userService;
+
+    @MockBean
+    LoginRequest loginRequest;
 
     @Test
     @DisplayName("Password must be converted to jwt token before saving")
@@ -41,5 +50,13 @@ public class UserServiceTest {
         Assert.assertNotNull(foundUser);
         verify(userRepository, times(1)).findByEmail(anyString());
 
+    }
+
+    @Test
+    @DisplayName("user account must be active to login")
+    public void userAccountMustBeActiveToLogin() {
+        User loginRequest = new User("tim", "tim@hotmail.com", "tim123");
+        when(userRepository.findByEmail(anyString())).thenReturn(loginRequest);
+        Assert.assertTrue(loginRequest.getActive());
     }
 }
