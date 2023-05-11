848ca4a9e750571265b646625415115833aba2ad
Jeff Ou <ojn2014520@gmail.com>
2023-05-11 11:32:39 -0500

    Create test cases for createPlaylist method from PlaylistService

--- src/main/java/com/audiogalaxy/audiogalaxy/service/PlaylistService.java
+++ src/main/java/com/audiogalaxy/audiogalaxy/service/PlaylistService.java
@@ -25,9 +25,10 @@ public class PlaylistService {
      * @throws InformationInvalidException if the name of playlist is blank
      */
     public Playlist createPlayList(Playlist playlist) {
-        if (playlist.getName() == null & playlist.getName().isBlank()) {
+        if (playlist.getName() == null || playlist.getName().isBlank()) {
             throw new InformationInvalidException("The name of playlist can not empty or contains only space");
         }
+        playlist.setUser(userContext.getCurrentLoggedInUser());
         // saving the playlist to database
         return playlistRepository.save(playlist);
     }
