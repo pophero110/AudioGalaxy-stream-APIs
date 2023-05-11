2a1c4f69639bbb47135af1a8a02295477bf24f6a
Jeff Ou <ojn2014520@gmail.com>
2023-05-11 11:34:15 -0500

    Create TestSecurityConfiguration for test environment

---
+++ src/test/java/com/audiogalaxy/audiogalaxy/TestSecurityConfiguratoin.java
@@ -0,0 +1,31 @@
+package com.audiogalaxy.audiogalaxy;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+import org.springframework.security.web.SecurityFilterChain;
+
+
+/**
+ * make the endpoint to be public in testing environment
+ */
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class TestSecurityConfiguratoin {
+
+    @Bean
+    public SecurityFilterChain testFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers(
+                        "/**"
+                ).permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable();
+
+        return http.build();
+    }
+}
