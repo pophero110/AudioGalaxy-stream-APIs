206ed6621a883c7bb0c313c91bcc8720b375e1b9
Jiajin Ou <jiajin.ou@chowbus.com>
2023-05-12 15:07:18 -0500

    Create GET request for retrieving a list of songs from a playlist

--- src/main/java/com/audiogalaxy/audiogalaxy/controller/PlaylistController.java
+++ src/main/java/com/audiogalaxy/audiogalaxy/controller/PlaylistController.java
@@ -1,7 +1,9 @@
 package com.audiogalaxy.audiogalaxy.controller;
 
 
+import com.audiogalaxy.audiogalaxy.exception.InformationNotFoundException;
 import com.audiogalaxy.audiogalaxy.model.Playlist;
+import com.audiogalaxy.audiogalaxy.model.Song;
 import com.audiogalaxy.audiogalaxy.service.PlaylistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +24,7 @@ public class PlaylistController {
 
     /**
      * Create a playlist and return the created playlist
+     *
      * @param playlist the object contains required data for creating a playlist
      * @return created playlist
      */
@@ -32,11 +35,24 @@ public class PlaylistController {
 
     /**
      * Return a list of playlists that belong to currently authenticated user
+     *
      * @return a list of playlists
      * @return 400 if the user does not have any playlist
      */
     @GetMapping(path = "/playlists/")
     public List<Playlist> getPlaylists() {
         return playlistService.getPlaylists();
+    }
+
+    /**
+     * Retrieves the list of songs associated with a playlist based on the provided playlist ID.
+     *
+     * @param playlistId The ID of the playlist for which to retrieve the songs.
+     * @return The list of songs associated with the specified playlist.
+     * @throws InformationNotFoundException if the playlist with the given ID is not found.
+     */
+    @GetMapping(path = "/playlists/{playlistId}/")
+    public List<Song> getSongsByPlaylist(@PathVariable Long playlistId) throws InformationNotFoundException {
+        return playlistService.getSongByPlaylistId(playlistId);
     }
 }
