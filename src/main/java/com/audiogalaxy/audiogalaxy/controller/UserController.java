ccdd2752dcd5d499f741a5b21400725be58e64a6
Jiajin Ou <jiajin.ou@chowbus.com>
2023-05-12 11:16:14 -0500

    Update docstring on UserController

--- src/main/java/com/audiogalaxy/audiogalaxy/controller/UserController.java
+++ src/main/java/com/audiogalaxy/audiogalaxy/controller/UserController.java
@@ -32,21 +32,34 @@ public class UserController {
     }
 
     /**
-     * Method calls loginUser method to validate user.
-     * @param loginRequest
-     * @return 200 and object which contains the token
-     * @return 400
-     * @throws Exception
+     * Authenticates a user with the provided login credentials and generates a JWT token upon successful authentication.
+     *
+     * Accepts a POST request with a JSON payload containing the user's login credentials (email and password). The method delegates
+     * the authentication process to the UserService's loginUser method, passing the LoginRequest object.
+     *
+     * If the authentication is successful, the method returns a ResponseEntity with the generated JWT token and an HTTP status of OK (200).
+     * The JWT token can be used for subsequent authorized requests.
+     *
+     * @param loginRequest The LoginRequest object containing the user's email and password.
+     * @return ResponseEntity with the generated JWT token upon successful authentication.
+     * @throws Exception if an error occurs during the authentication process.
      */
     @PostMapping(path="/users/login/")
     public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest) throws Exception {
         return ResponseEntity.ok(userService.loginUser(loginRequest));
     }
 
+    /**
+     * Deactivates the currently logged-in user.
+     *
+     * Sets the active status of the currently logged-in user to inactive. The user's account will be marked as inactive in the system.
+     * The method delegates the deactivation process to the UserService's setUserToInactive method and returns the result as a ResponseEntity.
+     *
+     * @return ResponseEntity with HTTP status OK (200) if the user is successfully set to inactive.
+     * @throws Exception if an error occurs during the deactivation process.
+     */
     @PutMapping(path="/users/deactivate/")
     public ResponseEntity<?> setUserToInactive() throws Exception {
         return ResponseEntity.ok(userService.setUserToInactive());
     }
-
-
 }
