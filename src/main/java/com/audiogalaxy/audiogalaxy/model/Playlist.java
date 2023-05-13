8b36062364f254aee07435987dd56b82df383d3b
Jiajin Ou <jiajin.ou@chowbus.com>
2023-05-12 20:55:40 -0500

    Fix infinite query loop by @JsonIgnore

--- src/main/java/com/audiogalaxy/audiogalaxy/model/Playlist.java
+++ src/main/java/com/audiogalaxy/audiogalaxy/model/Playlist.java
@@ -1,5 +1,7 @@
 package com.audiogalaxy.audiogalaxy.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +22,7 @@ public class Playlist {
 
     @ManyToOne
     @JoinColumn(name = "user_id")
+    @JsonIgnore
     private User user;
 
     @ManyToMany
