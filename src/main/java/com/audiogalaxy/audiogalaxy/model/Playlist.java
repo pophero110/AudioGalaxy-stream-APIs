8633675f5cbff94dfc001c56179aea44ed0c06bf
Jiajin Ou <jiajin.ou@chowbus.com>
2023-05-14 14:54:01 -0500

    Add setter methods for name and description for Playlist model

--- src/main/java/com/audiogalaxy/audiogalaxy/model/Playlist.java
+++ src/main/java/com/audiogalaxy/audiogalaxy/model/Playlist.java
@@ -71,8 +71,16 @@ public class Playlist {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public Long getId() {
